649e503ed8705f6d4b73eaad6554c074fbdf0831
gitubanana <ptyolo666@gmail.com>
2023-11-10 20:42:40 +0900

    Feat: url이 write -> write 함수

--- inflearn/community/views.py
+++ inflearn/community/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from community.forms import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def write(request):
+    if request.method == 'POST':
+        form = Form(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = Form()
+
+    return render(request, 'write.html', {'form' : form})
 
--- inflearn/tutorial/urls.py
+++ inflearn/tutorial/urls.py
@@ -21,4 +21,5 @@ from community.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('write/', write, name='write'),
 ]
