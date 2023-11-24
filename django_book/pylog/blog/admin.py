a7a522c72176bd0244aa6598286a88db6ad7cb83
gitubanana <ptyolo666@gmail.com>
2023-11-24 21:57:04 +0900

    Feat: admin 페이지에 model 추가

--- django_book/pylog/blog/admin.py
+++ django_book/pylog/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from blog.models import *
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "thumbnail"]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
