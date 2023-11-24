4e89c01a3bd94cf3b9e054d4942c2eeea958c586
gitubanana <ptyolo666@gmail.com>
2023-11-24 22:00:54 +0900

    Feat: 각 url에 맞게 함수 추가 - '' -> index - 'posts/' -> post_list - 'posts/<int:post_id>' -> post_detail

--- django_book/pylog/config/urls.py
+++ django_book/pylog/config/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from config.views import *
+from blog.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('posts/', post_list),
+    path('posts/<int:post_id>/', post_detail),
 ]
 
+urlpatterns += static(
+    prefix = settings.MEDIA_URL,
+    document_root = settings.MEDIA_ROOT,
+)
