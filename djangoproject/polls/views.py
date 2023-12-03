c43e6032da14b12f13cabe28199bab502eadcdda
gitubanana <ptyolo666@gmail.com>
2023-12-03 18:03:58 +0900

    Comment: 주석 수정 - 현재도 추가

--- djangoproject/polls/views.py
+++ djangoproject/polls/views.py
@@ -16,7 +16,7 @@ class IndexView(generic.ListView):
         Return the last five published questions
         (not including those set to be published in the future).
         """
-        # pub_date less than now
+        # pub_date <= now
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
