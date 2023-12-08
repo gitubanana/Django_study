51d2af768e8a423dc9b2e6f0e58c7fab71c622b3
gitubanana <ptyolo666@gmail.com>
2023-12-08 17:05:01 +0900

    Feat: 로그인이 안 되면, 로그인 페이지로 리다이렉션

--- django_book/pystagram/users/views.py
+++ django_book/pystagram/users/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render, redirect
 
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('/posts/feeds')
     return render(request, 'users/login.html')
