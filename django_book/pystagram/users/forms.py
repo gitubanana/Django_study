17319df73eba075a833e302de19d91b7a5afc2ab
gitubanana <ptyolo666@gmail.com>
2023-12-09 23:29:33 +0900

    Feat: SignupForm 클래스 - clean_{필드명} 메서드가 필드명이 타당한지 확인한다. - clean 메서드는 전체적인 데이터가 타당한지 확인한다.

--- django_book/pystagram/users/forms.py
+++ django_book/pystagram/users/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from users.models import User
+from django.core.exceptions import ValidationError
 
 
 class LoginForm(forms.Form):
@@ -11,6 +13,51 @@ class LoginForm(forms.Form):
     password = forms.CharField(
         min_length=4,
         widget=forms.PasswordInput(
-            attrs={'placeholder': '비밀번호(4자리 이상)'}
-        )
+            attrs={'placeholder': '비밀번호(4자리 이상)'},
+        ),
     )
+
+
+class SignupForm(forms.Form):
+    username = forms.CharField(
+        min_length=3,
+        widget=forms.TextInput(
+            attrs={'placeholder': '사용자명(3자리 이상)'},
+        ),
+    )
+    password1 = forms.CharField(
+        min_length=4,
+        widget=forms.PasswordInput(
+            attrs={'placeholder': '비밀번호(4자리 이상)'},
+        ),
+    )
+    password2 = forms.CharField(widget=forms.PasswordInput)
+    profile_image = forms.ImageField()
+    short_description = forms.CharField()
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise ValidationError(f'입력한 사용자명 {username}은 이미 사용 중입니다.')
+        return username
+
+    def clean(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        if password1 != password2:
+            self.add_error('비밀번호와 비밀번호 확인란이 다릅니다.')
+
+    def save(self):
+        username = self.cleaned_data['username']
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+        profile_image = self.cleaned_data['profile_image']
+        short_description = self.cleaned_data['short_description']
+        user = User.objects.create_user(
+            username=username,
+            password1=password1,
+            password2=password2,
+            profile_image=profile_image,
+            short_description=short_description,
+        )
+        return user
