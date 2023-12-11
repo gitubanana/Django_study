269b62d191943cc18f33a9e3083dd52cfd29e586
gitubanana <ptyolo666@gmail.com>
2023-12-11 13:27:02 +0900

    Feat: admin에서 모델 보기

--- django_book/pystagram/posts/admin.py
+++ django_book/pystagram/posts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Post, PostImage, Comment
+import admin_thumbnails
 
-# Register your models here.
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+
+@admin_thumbnails.thumbnail("photo")
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            "Post",
+            {
+                "fields": ["user", "content"],
+            }
+        ),
+    ]
+    inlines = [
+        CommentInline,
+        PostImageInline,
+    ]
+    list_display = [
+        "user",
+        "content",
+        "created",
+    ]
+    search_fields = ["content"]
