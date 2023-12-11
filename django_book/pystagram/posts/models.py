1594c520cdd37713a4ff12c1e66daa08aba63f5f
gitubanana <ptyolo666@gmail.com>
2023-12-11 13:26:03 +0900

    Feat: PostImage 모델

--- django_book/pystagram/posts/models.py
+++ django_book/pystagram/posts/models.py
@@ -9,3 +9,13 @@ class Post(models.Model):
     )
     content = models.TextField("내용")
     created = models.DateTimeField("생성일시", auto_now_add=True)
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(
+        Post,
+        verbose_name="포스트",
+        on_delete=models.CASCADE,
+    )
+    photo = models.ImageField("사진", upload_to="post")
+
