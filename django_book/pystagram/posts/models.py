6a93c6dc01b74aba6d9f0d1aea1195e8a390a527
gitubanana <ptyolo666@gmail.com>
2023-12-11 13:25:48 +0900

    Feat: Post 모델

--- django_book/pystagram/posts/models.py
+++ django_book/pystagram/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        verbose_name="작성자",
+        on_delete=models.CASCADE,
+    )
+    content = models.TextField("내용")
+    created = models.DateTimeField("생성일시", auto_now_add=True)
