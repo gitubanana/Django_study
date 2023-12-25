f57692756eef9641ddcf05930f8fd6c14cf68574
gitubanana <ptyolo666@gmail.com>
2023-12-25 19:57:07 +0900

    Feat: Follower와 Following을 admin 페이지에 표시

--- django_book/pystagram/users/admin.py
+++ django_book/pystagram/users/admin.py
@@ -3,6 +3,22 @@ from django.contrib.auth.admin import UserAdmin
 from users.models import *
 
 
+class FollowersInline(admin.TabularInline):
+    model = User.following.through
+    fk_name = "from_user"
+    verbose_name = "내가 팔로우 하고 있는 사용자"
+    verbose_name_plural = f"{verbose_name} 목록"
+    extra = 1
+
+
+class FollowingInline(admin.TabularInline):
+    model = User.following.through
+    fk_name = "to_user"
+    verbose_name = "나를 팔로우 하고 있는 사용자"
+    verbose_name_plural = f"{verbose_name} 목록"
+    extra = 1
+
+
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
     fieldsets = [
@@ -21,4 +37,8 @@ class CustomUserAdmin(UserAdmin):
         ),
         ('중요한 일정', {'fields': ('last_login', 'date_joined')}),
         ('연관객체', {"fields": ('like_posts', )}),
+    ]
+    inlines = [
+        FollowersInline,
+        FollowingInline,
     ]
